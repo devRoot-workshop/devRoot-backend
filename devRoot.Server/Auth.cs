dc5be270da748e7cd729e46f4e67069f378b5103
Solymosi Tamás <csaksway@gmail.com>
2024-11-26 20:03:03 +0100

    firebase-login

---
+++ devRoot.Server/Auth.cs
@@ -0,0 +1,49 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+
+namespace devRoot.Server
+{
+    public class FirebaseService
+    {
+        public FirebaseService()
+        {
+            FirebaseApp.Create(new AppOptions
+            {
+                Credential = GoogleCredential.FromFile("./devRoot.json")
+            });
+        }
+    }
+
+    public class FirebaseAuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FirebaseAuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var authHeader = context.Request.Headers["Authorization"].ToString();
+
+            if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Bearer "))
+            {
+                var token = authHeader.Substring("Bearer ".Length).Trim();
+                try
+                {
+                    var decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(token);
+                    context.Items["User"] = decodedToken;
+                }
+                catch (Exception)
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
