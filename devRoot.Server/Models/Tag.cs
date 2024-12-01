318f7e154157fb3beaeb1a4fda3e0f543fb4ef94
Solymosi Tamás <csaksway@gmail.com>
2024-12-01 13:22:26 +0100

    fix firebaseauth attribute

---
+++ devRoot.Server/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace devRoot.Server.Models
+{
+    public class Tag : TagRequest
+    {
+        public int? ID { get; set; }
+    }
+
+    public class TagRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
